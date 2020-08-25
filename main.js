3d45616dd2b0a5f912c362f31d63b8a79b320039
isolatedYeti <isolatedYeti@users.noreply.github.com>
2020-08-24 20:12:56 -0400

    Replaced bad newlines
    changes \n's to visual line breaks to prevent third party tools used on the files from bugging out, and from Sherry not responding to commands that start with a prefix.

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 const {token, prefix} = require("./config.json");const Discord = require("discord.js");const client = new Discord.Client();
 client.on("ready", () => {console.log(`Bot ${client.user.tag}! logged in`);});
-client.on("message", msg => { if (message.author.bot) return; if (msg.content === `${prefix}ping`) {msg.reply("Sherry is Alive! Set the command prefix with \".pre !\" Use command \"commands\" to view the current capabilities of Sherry!");}});client.login(token);\n
+client.on("message", msg => { if (message.author.bot) return; if (msg.content === `${prefix}ping`) {msg.reply("Sherry is Alive! Set the command prefix with \".pre !\" Use command \"commands\" to view the current capabilities of Sherry!");}});client.login(token);
